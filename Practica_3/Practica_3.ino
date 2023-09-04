736a8f5000b73f1809dcff4606c0a5e73d203546
Ricardo_Portatil <ricardo.macias1755@alumnos.udg.mx>
2023-09-04 17:35:50 -0600

    feat: agregue setup y void en Practica_3

--- Practica_3/Practica_3.ino
+++ Practica_3/Practica_3.ino
@@ -1,9 +1,39 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); //Pines a utilizar
+
 void setup() {
-  // put your setup code here, to run once:
+  lcd.begin(16,2); //columna 16 fila 2
+  lcd.print("ARDUINO");
+  delay(1000); //Pausa
+  lcd.clear(); //Limpiar lcd
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  lcd.clear();
+  lcd.setCursor(0,0);  //Cursor en la posocopm 0 en el lcd
+  lcd.print("Probador de pilas");
+  lcd.setCursor(0,1);
+  lcd.print(analogRead(0)*5.00/1024); //Comvertir a Volage
+  lcd.print("V=>");
+  lcd.setCursor(8,1); 
+  
+  //Medir
+  if((analogRead(0)*5.00/1023.00)>1.40){  
+    lcd.print("Perfecto");
+  }
+  else if((analogRead(0)*5.00/1023.00)>1.20 && (analogRead(0)*5.00/1023.00)<1.40) {
+    lcd.print("Bueno");
+  }
+  else if((analogRead(0)*5.00/1023.00)<1.20 && (analogRead(0)*5.00/1023.00)>0.30){
+    lcd.print("Regular");
+  }
+  else if((analogRead(0)*5.00/1023.00)<0.3){
+    lcd.print("Malo");
+  }
+  else{
+    lcd.print("");
+  }
+  delay(250);
 }
