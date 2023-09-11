eb7ca32627c894cb915de67b861180db1c7706c2
Ricardo_Portatil <ricardo.macias1755@alumnos.udg.mx>
2023-09-11 16:47:58 -0600

    feat: conectar lcd16x2

--- Practica_4/Practica_4.ino
+++ Practica_4/Practica_4.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h>
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(16,2);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Temperatura");
+  lcd.setCursor(0,5);
+  lcd.print(analogRead(4));
+  delay(3000);
+
+
+
 
 }
