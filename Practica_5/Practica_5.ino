f349b94269ed61a73f61a10ad74ecc691285e3a1
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-09-18 15:37:32 -0600

    feat: Inicializar comunicacion

--- Practica_5/Practica_5.ino
+++ Practica_5/Practica_5.ino
@@ -1,6 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define Pin 2
 
+OneWire ourWire(Pin);
+DallasTemperature sensors(&ourWire);
+
+void setup() {
+  delay(1000);
+  Serial.begin(9600);
+  sensors.begin();
 }
 
 void loop() {
