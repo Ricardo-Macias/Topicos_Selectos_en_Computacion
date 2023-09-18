29b623e1918ca60af6aaa0e01e598f1633a237db
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-09-18 16:10:07 -0600

    feat: conectar el lcd

--- Practica_5/Practica_5.ino
+++ Practica_5/Practica_5.ino
@@ -1,13 +1,16 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <LiquidCrystal.h>
 #define Pin 2
 
 OneWire ourWire(Pin);
 DallasTemperature sensors(&ourWire);
+LiquidCrystal lcd(12,11,7,6,5,4);
 
 void setup() {
   delay(1000);
   Serial.begin(9600);
+  lcd.begin(16,2);
   sensors.begin();
 }
 
@@ -17,6 +20,11 @@ void loop() {
   Serial.print(sensors.getTempCByIndex(0));
   Serial.println(" Grados centigrados");
 
-  delay(1000)
+  lcd.setCursor(0,0);
+  lcd.print("Temp: ");
+  lcd.print(sensors.getTempCByIndex(0));
+  lcd.print(" C");
+
+  delay(1000);
 
 }
