5179848e4addf7d7f638aa47863e7bc3f6795425
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-09-23 14:15:21 -0600

    feat: configurar keypad

--- Practica_6/Practica_6.ino
+++ Practica_6/Practica_6.ino
@@ -1,13 +1,29 @@
-#include <Keypad.h>
+#include <Keypad.h> //Importar libreria 
 
-const 
+const byte Row = 4; //Numero de filas del teclado
+const byte column = 4; //Numero de columnas del teclado
+
+char keys[Row][column] = {
+  {"1", "2", "3", "A"},
+  {"4", "5", "6", "B"},
+  {"7", "8", "9", "C"},
+  {"*", "0", "#", "D"}
+}; //Definir un mapa de la distribucion del teclado
+
+byte pinesRow[Row] = {7, 6, 5, 4}; //pines donde esta conectado
+byte pinesColumn[Column] = {3, 2, 1, 0}; //pines donde esta conectado
+
+Keypad teclado = Keypad(makeKeymap(keys), pinesRow, pinesColumn, Row, Column);
+
+char tecla;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  tecla = teclado.getKey();
+  Serial.print(tecla);
+  delay(1000);
 }
