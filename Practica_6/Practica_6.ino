0b7952a43239b48bf62022ff86ed868cb4984037
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-09-23 14:30:44 -0600

    fix: corregir variable colum y keys

--- Practica_6/Practica_6.ino
+++ Practica_6/Practica_6.ino
@@ -1,13 +1,13 @@
 #include <Keypad.h> //Importar libreria 
 
 const byte Row = 4; //Numero de filas del teclado
-const byte column = 4; //Numero de columnas del teclado
+const byte Column = 4; //Numero de columnas del teclado
 
-char keys[Row][column] = {
-  {"1", "2", "3", "A"},
-  {"4", "5", "6", "B"},
-  {"7", "8", "9", "C"},
-  {"*", "0", "#", "D"}
+char keys[Row][Column] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 }; //Definir un mapa de la distribucion del teclado
 
 byte pinesRow[Row] = {7, 6, 5, 4}; //pines donde esta conectado
