bf8f63357ea5469b12091e6929623ba939cd1c5f
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-02 14:05:30 -0600

    feat: agregar funcion receiveEvent en Slave

--- Practica_7_Master_Slave/Slave/Slave.ino
+++ Practica_7_Master_Slave/Slave/Slave.ino
@@ -7,8 +7,12 @@ int x = 0;
 void setup() {
   pinMode(LED, OUTPUT);
   Wire.begin(9); //Arranca el bus I2C como esclavo en la direcciob 9.
-  Wire.onReceive() //Conecta una funcion para disparar cuando se recibe algo.
+  Wire.onReceive(receiveEvent) //Conecta una funcion para disparar cuando se recibe algo.
 
+}
+
+void receiveEvent(int bytes){
+  x = Wire.read(); //Lee un caracter del I2C.
 }
 
 void loop() {
