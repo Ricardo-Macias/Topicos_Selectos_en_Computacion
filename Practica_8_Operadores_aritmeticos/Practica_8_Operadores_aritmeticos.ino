bc0ba8ddbfa39713c569277f053da4c067b8410b
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-15 16:57:34 -0600

    feat: agregar lcd

--- Practica_8_Operadores_aritmeticos/Practica_8_Operadores_aritmeticos.ino
+++ Practica_8_Operadores_aritmeticos/Practica_8_Operadores_aritmeticos.ino
@@ -1,6 +1,11 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Keypad.h>
+#include <LiquidCrystal.h>
 
+LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
+
+
+void setup() {
+  lcd.begin(16,2);
 }
 
 void loop() {
