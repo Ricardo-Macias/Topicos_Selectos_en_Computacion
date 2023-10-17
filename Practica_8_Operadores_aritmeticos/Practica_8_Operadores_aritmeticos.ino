c6dd170fd9c815c14e8bcbee50b943fa8d656344
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-16 18:31:01 -0600

    feat: agregar operaciones aritmeticas

--- Practica_8_Operadores_aritmeticos/Practica_8_Operadores_aritmeticos.ino
+++ Practica_8_Operadores_aritmeticos/Practica_8_Operadores_aritmeticos.ino
@@ -1,7 +1,7 @@
 #include <Keypad.h>
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
+LiquidCrystal lcd(13,12,11,10,9,8);
 const byte Row = 4;
 const byte Column = 4;
 
@@ -12,20 +12,96 @@ char keys[Row][Column] = {
   {'*','0','#','D'}
 };
 
-byte pinesRow = {7,6,5,4};
-byte pinesColumn = {3,2,1,0};
+byte pinesRow[Row] = {7,6,5,4};
+byte pinesColumn[Column] = {3,2,1,0};
 
-Keypad teclado = Keypad(makeKeymap(keys),pinesRow, pinesColumn, Row, Colummn);
+Keypad teclado = Keypad(makeKeymap(keys),pinesRow, pinesColumn, Row, Column);
 
-char tecla;
+char var, option;
+String digito_1, digito_2;
+float x, y;
 
 
 void setup() {
   lcd.begin(16,2);
-
+  Serial.begin(9600);
+  lcd.print("ARDUINO");
+  delay(1500);
 }
 
 void loop() {
-  
+  lcd.clear();
+  lcd.print("Primer digito: ");
+  lcd.setCursor(0, 1);
+
+  while (var != '*'){
+    var = teclado.getKey();
+    if (var != NULL && var != '*'){
+      lcd.print(var);
+      digito_1 += var;
+    }
+  }
+  x = digito_1.toFloat();
+
+  lcd.clear();
+  lcd.print("Segundo digito");
+  lcd.setCursor(0, 1);
+  var = "";
+
+  while (var != '*'){
+    var = teclado.getKey();
+    if (var != NULL && var != '*'){
+      lcd.print(var);
+      digito_2 += var;
+    }
+  }
+  y = digito_2.toFloat();
+  var = "";
+
+  lcd.clear();
+  lcd.print("Operacion: ");
+
+  do {
+    option = teclado.getKey();
+    delay(50);
+    switch(option){
+      case '1':
+        lcd.print(digito_1);
+        lcd.print(" + ");
+        lcd.print(digito_2);
+        lcd.print(" = ");
+        lcd.setCursor(0,1);
+        lcd.print( x + y);
+        break;
+      case '2':
+        lcd.print(digito_1);
+        lcd.print(" + ");
+        lcd.print(digito_2);
+        lcd.print(" = ");
+        lcd.setCursor(0,1);
+        lcd.print( x - y);
+        break;
+      case '3':
+        lcd.print(digito_1);
+        lcd.print(" + ");
+        lcd.print(digito_2);
+        lcd.print(" = ");
+        lcd.setCursor(0,1);
+        lcd.print( x * y);
+        break;
+      case '4':
+        lcd.print(digito_1);
+        lcd.print(" + ");
+        lcd.print(digito_2);
+        lcd.print(" = ");
+        lcd.setCursor(0,1);
+        lcd.print( x / y);
+        break;
+      default:
+        break;
+    }
+  }while(option == NO_KEY);
+
+  delay(3000);
 
 }
