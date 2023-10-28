696f6545ac3c90e2ed494942fcb05610a89530d4
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-27 21:29:09 -0600

    feat: agregar if por si detecta movimiento

--- Practica_9_Alarma_con_sensor_de_movimiento_PIR_y_buzzer/Practica_9_Alarma_con_sensor_de_movimiento_PIR_y_buzzer.ino
+++ Practica_9_Alarma_con_sensor_de_movimiento_PIR_y_buzzer/Practica_9_Alarma_con_sensor_de_movimiento_PIR_y_buzzer.ino
@@ -1,10 +1,9 @@
 int sensorPIR = 11;
 int pinBuzzer = 9;
-int pinLed = 12
+int pinLed = 12;
 float mi = 329.63;
 
 void setup() {
-  Serial.begin(9600);
   pinMode(sensorPIR, INPUT);
   pinMode(pinBuzzer, OUTPUT);
   pinMode(pinLed, OUTPUT);
@@ -13,10 +12,17 @@ void setup() {
 
 void loop() {
   int valor = digitalRead(sensorPIR);
-  Serial.println(valor);
   if(valor == 1){
-
+    digitalWrite(pinLed, HIGH);
+    for(int count=0; count < 3; count++){
+      tone(pinBuzzer,mi);
+      delay(250);
+      noTone(pinBuzzer);
+      delay(250);
+    }
   }
-  
-
+  else{
+    digitalWrite(pinLed, LOW);
+  }
+  delay(250);
 }
