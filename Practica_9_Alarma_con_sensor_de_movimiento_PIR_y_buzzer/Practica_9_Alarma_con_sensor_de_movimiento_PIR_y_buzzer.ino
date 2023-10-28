7f08e86dc3c8b053580a14fb36726e88d884a5a2
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-27 19:40:32 -0600

    feat: conectar buzzer

--- Practica_9_Alarma_con_sensor_de_movimiento_PIR_y_buzzer/Practica_9_Alarma_con_sensor_de_movimiento_PIR_y_buzzer.ino
+++ Practica_9_Alarma_con_sensor_de_movimiento_PIR_y_buzzer/Practica_9_Alarma_con_sensor_de_movimiento_PIR_y_buzzer.ino
@@ -1,12 +1,18 @@
 int sensorPIR = 12;
+int pinBuzzer = 9;
+float mi = 329.63;
 
 void setup() {
   Serial.begin(9600);
   pinMode(sensorPIR, INPUT);
+  pinMode(pinBuzzer, OUTPUT);
 
 }
 
 void loop() {
   int valor = digitalRead(sensorPIR);
   Serial.println(valor);
+  delay(250);
+  
+
 }
