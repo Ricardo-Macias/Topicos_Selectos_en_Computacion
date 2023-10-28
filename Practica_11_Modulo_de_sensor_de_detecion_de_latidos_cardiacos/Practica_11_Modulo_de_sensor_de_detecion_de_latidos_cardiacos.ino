3684c4302ad1ee41ad31f3f5f8145dfbdbef0eb1
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-28 15:37:58 -0600

    feat: hacer las medidas

--- Practica_11_Modulo_de_sensor_de_detecion_de_latidos_cardiacos/Practica_11_Modulo_de_sensor_de_detecion_de_latidos_cardiacos.ino
+++ Practica_11_Modulo_de_sensor_de_detecion_de_latidos_cardiacos/Practica_11_Modulo_de_sensor_de_detecion_de_latidos_cardiacos.ino
@@ -1,24 +1,49 @@
 int sensorPin = 0;
+int latidos = 0;
+int rawValue;
+
 double alpha = 0.75;
-int period = 100;
-double change = 0.0;
 double minval= 0.0;
+double value;
+double cambio;
+
+float antes = 512;
+float maxval = 0.0;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  static double oldValue = 0;
-  static double oldChange = 0;
+  static unsigned long tiempoLMP = millis();
+  static unsigned long latidosLPM = millis();
 
-  int rawValue = analogRead(sensorPin);
-  double value = alpha * oldValue + (1 - alpha) * rawValue;
+  rawValue = analogRead(sensorPin);
 
-  Serial.print(rawValue);
-  Serial.print(",");
-  Serial.println(value);
-  oldValue = value;
-  delay(period);
+  value = alpha * antes + (1 - alpha) * rawValue;
+  cambio = value - antes;
+  antes = value;
+
+  if((cambio >= maxval) && (millis() > latidosLPM + minval)){
+    maxval = cambio;
+    digitalWrite(13,HIGH);
+    latidosLPM = millis();
+    latidos++;
+  }
+  else{
+    digitalWrite(13,LOW);
+  }
+  maxval = maxval * 0.97;
+
+  if(millis() >= tiempoLMP + 10000){
+    int L = latidos*6;
+    Serial.print("Latidos: ");
+    Serial.println(L);
+
+    latidos = 0;
+    tiempoLMP = millis();
+  }
+
+ delay(50);
 
 }
