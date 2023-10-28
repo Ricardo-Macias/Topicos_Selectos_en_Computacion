80917ab93d13a709f3482813df5bdc34c09339f2
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-28 13:15:11 -0600

    feat: conectar sensor

--- Practica_11_Modulo_de_sensor_de_detecion_de_latidos_cardiacos/Practica_11_Modulo_de_sensor_de_detecion_de_latidos_cardiacos.ino
+++ Practica_11_Modulo_de_sensor_de_detecion_de_latidos_cardiacos/Practica_11_Modulo_de_sensor_de_detecion_de_latidos_cardiacos.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+int sensorPin = 0;
+double alpha = 0.75;
+int period = 100;
+double change = 0.0;
+double minval= 0.0;
 
+void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  static double oldValue = 0;
+  static double oldChange = 0;
+
+  int rawValue = analogRead(sensorPin);
+  double value = alpha * oldValue + (1 - alpha) * rawValue;
+
+  Serial.print(rawValue);
+  Serial.print(",");
+  Serial.println(value);
+  oldValue = value;
+  delay(period);
 
 }
