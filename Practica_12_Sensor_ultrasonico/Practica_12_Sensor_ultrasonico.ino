0bbd4f89c83d24b0cab0f099ce98b8ae63b9639d
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-10-30 18:54:58 -0600

    feat: convertir a cm y m

--- Practica_12_Sensor_ultrasonico/Practica_12_Sensor_ultrasonico.ino
+++ Practica_12_Sensor_ultrasonico/Practica_12_Sensor_ultrasonico.ino
@@ -1,10 +1,10 @@
 #include <LiquidCrystal.h>
 
+LiquidCrystal lcd(12,11,5,4,3,2);
 int trig = 9;
 int echo = 8;
 float value;
-
-LiquidCrystal lcd(12,11,5,4,3,2);
+float distancia;
 
 void setup() {
   pinMode(trig, OUTPUT);
@@ -14,18 +14,42 @@ void setup() {
   lcd.begin(2,16);
   lcd.print("Practica 12");
   delay(3000);
+  lcd.clear();
 
 }
 
 void loop() {
+  lcd.setCursor(0, 0);
+  lcd.print("Dist: ");
+  lcd.setCursor(7, 0);
   digitalWrite(trig,HIGH);
-  delayMicroseconds(10);
-
-  value = pulseIn(echo,HIGH);
-  delay(25);
+  delayMicroseconds(1000);
   digitalWrite(trig, LOW);
 
-  Serial.print("Distancia ");
-  Serial.println(value);
+  value = pulseIn(echo,HIGH);
+  delay(100);
+
+  distancia = (value/2)/29.1; //value * 10 / 292 / 2;
+
+  if(distancia < 500){
+    lcd.print(distancia/100);
+
+    Serial.print("Distancia ");
+    Serial.print(distancia/100);
+    if(distancia < 100){
+      lcd.print("cm");
+      Serial.println(" cm");
+    }
+    else if(distancia >= 100){
+      lcd.print(" m");
+      Serial.println(" m");
+    }
+  }
+  else{
+    lcd.print("FUERA DE RANGO");
+    Serial.println("FUERA DE RANGO");
+    delay(500);
+    lcd.clear();
+  }
 
 }
