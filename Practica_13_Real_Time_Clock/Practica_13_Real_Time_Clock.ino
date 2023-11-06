797a20c32d950ba2b91b526273d0cdd96e3dd29b
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-11-05 22:17:17 -0600

    feat: conectar pantalla lcd

--- Practica_13_Real_Time_Clock/Practica_13_Real_Time_Clock.ino
+++ Practica_13_Real_Time_Clock/Practica_13_Real_Time_Clock.ino
@@ -1,25 +1,43 @@
 #include <RTClib.h>
+#include <LiquidCrystal.h>
 
 RTC_DS1307 rtc;
 
+LiquidCrystal lcd(12,11,5,4,3,2);
+
 void setup() {
-  Serial.begin(9600);
+  lcd.begin(16,2);
 
   if(! rtc.begin()){
     Serial.print("No encontrado el RTC");
     while (1);
   }
 
-  rtc.adjust(DateTime(__DATE__,__TIME__));
+  //rtc.adjust(DateTime(__DATE__,__TIME__));
+  lcd.clear();
 }
 
 void loop() {
   DateTime fecha = rtc.now();
 
-  Serial.println("Fecha: " + fecha.timestamp(DateTime::TIMESTAMP_DATE));
-  Serial.println("Hora: " + fecha.timestamp(DateTime::TIMESTAMP_TIME));
+  lcd.setCursor(0, 0);
+  lcd.print("Fecha: ");
+  lcd.print(fecha.day());
+  lcd.print("/");
+  lcd.print(fecha.month());
+  lcd.print("/");
+  lcd.print(fecha.year());
 
+  lcd.setCursor(0, 1);
+  lcd.print("Hora: ");
+  lcd.print(fecha.hour());
+  lcd.print(":");
+  lcd.print(fecha.minute());
+  lcd.print(":");
+  if (fecha.second() < 10){
+    lcd.print("0");
+  }
+  lcd.print(fecha.second());
 
   delay(1000);
-
 }
