2ebd8e394ad5f1563bb5eb6decb0a9cf1a568fb6
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-11-05 18:40:04 -0600

    feat: conectar RTC

--- Practica_13_Real_Time_Clock/Practica_13_Real_Time_Clock.ino
+++ Practica_13_Real_Time_Clock/Practica_13_Real_Time_Clock.ino
@@ -1,7 +1,25 @@
-void setup() {
+#include <RTClib.h>
 
+RTC_DS1307 rtc;
+
+void setup() {
+  Serial.begin(9600);
+
+  if(! rtc.begin()){
+    Serial.print("No encontrado el RTC");
+    while (1);
+  }
+
+  rtc.adjust(DateTime(__DATE__,__TIME__));
 }
 
 void loop() {
+  DateTime fecha = rtc.now();
+
+  Serial.println("Fecha: " + fecha.timestamp(DateTime::TIMESTAMP_DATE));
+  Serial.println("Hora: " + fecha.timestamp(DateTime::TIMESTAMP_TIME));
+
+
+  delay(1000);
 
 }
