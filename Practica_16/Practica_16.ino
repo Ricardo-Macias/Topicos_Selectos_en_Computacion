c7ca3d90282977dd7057fa95cdc551cef97e9894
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2023-11-17 18:57:29 -0600

    feat: configurar LDR

--- Practica_16/Practica_16.ino
+++ Practica_16/Practica_16.ino
@@ -1,8 +1,14 @@
+int pinLDR = A0;
+int value;
 
 void setup() {
+  Serial.begin(9600);
+  pinMode(pinLDR,INPUT);
 
 }
 
 void loop() {
-
+  value = analogRead(pinLDR);
+  Serial.println(value);
+  delay(100);
 }
